bb92524dfb7d1e6be580a8f7688b5b30e815d693
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-20 18:56:53 -0300

    Cria a rota de home e perfil

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def homepage():
     return "FakePinterest - Uma réplica do Pinterest!"
 
+@app.route("/perfil")
+def perfil():
+    return "Página de perfil do Usuário."
+
 
 if __name__ == "__main__":
     app.run(debug=True)
