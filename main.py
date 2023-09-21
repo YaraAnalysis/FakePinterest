5a1b12bbe8ea65b20211e82637502ca10b5c6afd
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-21 11:24:32 -0300

    Cria rotas e páginas dinâmicas com render_template

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
-
+# o url_for permite pegar a url de acordo com o nome da função, e não com a rota dele.
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest - Uma réplica do Pinterest!"
+    return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return "Página de perfil do Usuário."
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
 
 
 if __name__ == "__main__":
