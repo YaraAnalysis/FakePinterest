ad77921892f7525540778bf611e91dc4ebb74ea4
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-21 14:06:46 -0300

    Organiza a estrutura de pastas e arquivos

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from flask import Flask, render_template, url_for
-
-# o url_for permite pegar a url de acordo com o nome da função, e não com a rota dele.
-app = Flask(__name__)
-
-@app.route("/")
-def homepage():
-    return render_template("homepage.html")
-
-@app.route("/perfil/<usuario>")
-def perfil(usuario):
-    return render_template("perfil.html", usuario=usuario)
+from fakePinterest import app
 
 
 if __name__ == "__main__":
