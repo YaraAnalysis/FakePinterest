c4658716c70b04c0126e8cbf3b1c8358f320661a
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-09-29 22:40:51 -0300

    Implementa o sistema de login e segurança com flask-login

--- fakePinterest/__init__.py
+++ fakePinterest/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "54b997395d20818371271c044c4ee159"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
 
 from fakePinterest import routes
--- fakePinterest/routes.py
+++ fakePinterest/routes.py
@@ -1,10 +1,14 @@
 from flask import render_template, url_for
 from fakePinterest import app
+from flask_login import login_required
+
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/perfil/<usuario>")
+@login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
